fa3e06101e84adcd9c9c7132a08bbfa84b6a1a82
aeiou879 <76179088+aeiou879@users.noreply.github.com>
2021-06-21 17:25:06 -0700

    Update list.js

--- commands/list.js
+++ commands/list.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const commands = [];
 
-fs.readdir(process.cwd() + '/mcCommands', function(err, res) {
+fs.readdir('../mcCommands', function(err, res) {
     var files = res.filter(filename => filename.startsWith('command'));
     files.forEach(filename => {
         commands.push(require('./' + filename));
