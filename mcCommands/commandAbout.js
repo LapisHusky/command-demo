78ec44b0d02cd3a3c1a377a11a2c9e8ab138ceb6
aeiou879 <76179088+aeiou879@users.noreply.github.com>
2021-06-21 17:23:15 -0700

    Delete mcCommands directory

--- mcCommands/commandAbout.js
+++
@@ -1,5 +0,0 @@
-exports.name = 'about';
-exports.permission = 'default';
-exports.eval = function() {
-    return 'This bot is made by LapisHusky. The best way to contact me about any questions or issues is by DMing me on Discord, Lapis#7110';
-}
--- mcCommands/commandHelp.js
+++
@@ -1,6 +0,0 @@
-exports.name = 'help';
-exports.permission = 'default';
-exports.aliases = ['commands'];
-exports.eval = function() {
-    return 'Commands: !help, !about, !seen, !playtime, !link, !unlink';
-}
